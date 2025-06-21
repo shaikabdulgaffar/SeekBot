1113eade4bd66fd702fcf21933404c8ee33ff89e
shaikabdulgaffar <ag2336818@gmail.com>
2025-06-21 22:21:18 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from deepseek_model_loader import chat_with_model
+from model_loader import chat_with_model
 
 app = Flask(__name__, static_folder="static", template_folder="template")
 CORS(app)
